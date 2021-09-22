59fc8bc14ccae21e91ff5fa07aef7f2e7fac1efc
BeADia <koc0819@gmail.com>
2021-09-22 17:08:22 +0900

    완주하지 못한 선수

---
+++ src/main/java/programmers/hash/hash1.java
@@ -0,0 +1,23 @@
+package programmers.hash;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class hash1 {
+    public static void main(String[] args) {
+        class Solution {
+            public String solution(String[] participant, String[] completion) {
+                String answer = "";
+                HashMap<String, Integer> hm = new HashMap<>();
+                for (String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
+                for (String player : completion) hm.put(player, hm.get(player) - 1);
+                for (String key : hm.keySet()) {
+                    if (hm.get(key) != 0) {
+                        answer = key;
+                    }
+                }
+                return answer;
+            }
+        }
+    }
+}
