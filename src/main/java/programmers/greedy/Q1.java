b077f11099b248883033a7131ca952f62ce143b6
BeADia <koc0819@gmail.com>
2021-11-11 16:32:46 +0900

    Greedy/체육복 수정

--- src/main/java/programmers/greedy/Q1.java
+++ src/main/java/programmers/greedy/Q1.java
@@ -1,47 +1,40 @@
 package programmers.greedy;
 
-import java.util.ArrayList;
-
-//contains 사용?
 public class Q1 {
     public static void main(String[] args) {
         class Solution {
             public int solution(int n, int[] lost, int[] reserve) {
-                ArrayList<Integer> possible = new ArrayList<>();
-                ArrayList<Integer> lostList = new ArrayList<>();
-                for (int x : lost)
-                    lostList.add(x);
-                ArrayList<Integer> reserveList = new ArrayList<>();
-                for (int x : reserve)
-                    reserveList.add(x);
+                int[] array = new int[n];
+                int result = n;
+                // 처음에 모두가 체육복을 가짐
                 for (int i = 0; i < n; i++) {
-                    int count = 1;
-                    if (lostList.contains(i + 1))
-                        count--;
-                    if (reserveList.contains(i + 1))
-                        count++;
-                    possible.add(count);
+                    array[i] = 1;
                 }
-                // 뒤 번호에게 빌리기
-                for (int i = 0; i < possible.size() - 1; i++) {
-                    if (possible.get(i) == 0 && possible.get(i + 1) == 2) {
-                        possible.set(i, 1);
-                        possible.set(i + 1, 1);
+                // lost 적용
+                for (int v : lost) {
+                    array[v - 1]--;
+                }
+                // reserve 적용
+                for (int v : reserve) {
+                    array[v - 1]++;
+                }
+                // 체육복 빌리기
+                for (int i = 0; i < array.length; i++) {
+                    if (array[i] == 0) {
+                        // 앞 사람에게 빌리기
+                        if (i - 1 >= 0 && array[i - 1] == 2) {
+                            array[i]++;
+                            array[i - 1]--;
+                            // 뒤 사람에게 빌리기
+                        } else if (i + 1 < array.length && array[i + 1] == 2) {
+                            array[i]++;
+                            array[i + 1]--;
+                        } else {
+                            result--;
+                        }
                     }
                 }
-                // 앞 번호에게 빌리기
-                for (int i = 1; i < possible.size(); i++) {
-                    if (possible.get(i) == 0 && possible.get(i - 1) == 2) {
-                        possible.set(i, 1);
-                        possible.set(i - 1, 1);
-                    }
-                }
-                int count = 0;
-                for (int val : possible) {
-                    if (val != 0)
-                        count++;
-                }
-                return count;
+                return result;
             }
         }
         Solution solution = new Solution();
@@ -51,26 +44,26 @@ public class Q1 {
         int[] lost = {2, 4};
         int[] reserve = {1, 3, 5};
         System.out.println(solution.solution(n, lost, reserve));
-        //case2
-        n = 5;
-        lost = new int[]{2, 4};
-        reserve = new int[]{3};
-        System.out.println(solution.solution(n, lost, reserve));
-        //case 3
-        n = 3;
-        lost = new int[]{3};
-        reserve = new int[]{1};
-        System.out.println(solution.solution(n, lost, reserve));
-        //case 4
-        n = 5;
-        lost = new int[]{1, 2, 3, 4, 5};
-        reserve = new int[]{1};
-        System.out.println(solution.solution(n, lost, reserve));
+//        case2
+//        n = 5;
+//        lost = new int[]{2, 4};
+//        reserve = new int[]{3};
+//        System.out.println(solution.solution(n, lost, reserve));
+//        case 3
+//        n = 3;
+//        lost = new int[]{3};
+//        reserve = new int[]{1};
+//        System.out.println(solution.solution(n, lost, reserve));
+//        case 4
+//        n = 5;
+//        lost = new int[]{1, 2, 3, 4, 5};
+//        reserve = new int[]{1};
+//        System.out.println(solution.solution(n, lost, reserve));
         //case 5
         n = 5;
         lost = new int[]{3, 5};
         reserve = new int[]{2, 4};
-        System.out.println(solution.solution(n, lost, reserve));
+        System.out.println(solution.solution(n, reserve, lost));
 
     }
 }
