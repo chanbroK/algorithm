2a5316abb92f7db7fb6958bccc7c1e407343ed89
chanbroK <koc0819@gmail.com>
2022-04-19 23:12:20 +0900

    kakao intern 2019 튜플

--- src/main/java/kakao/intern2019/Q2.java
+++ src/main/java/kakao/intern2019/Q2.java
@@ -1,12 +1,14 @@
 package kakao.intern2019;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Q2 {
     public static void main(String[] args) {
         String s = "{{2},{2,1},{2,1,3},{2,1,3,4}}";
 //        s = "{{1,2,3},{2,1},{1,2,4,3},{2}}";
-//        s = "{{20,111},{111}}";
+        s = "{{20,111},{111}}";
 //        s = "{{123}}";
 //        s = "{{4,2,3},{3},{2,3,4,1},{2,3}}";
 
@@ -21,30 +23,43 @@ public class Q2 {
         s = s.replace("},{", "}{");
         System.out.println("processed string = " + s);
         // 문자열 순회하면서 집합으로 변환
-        HashSet<HashSet<Integer>> totalSet = new HashSet<>();
-        HashSet<Integer> partSet = null;
-        Integer ele = 0;
+        ArrayList<ArrayList<Integer>> totalList = new ArrayList<>();
+        ArrayList<Integer> partList = null;
+        int ele = 0;
         for (int i = 0; i < s.length(); i++) {
-            String stringEle =
-            if (c == '{') {
+            String stringEle = s.substring(i, i + 1);
+            if (stringEle.equals("{")) {
                 // 집합 시작
-                partSet = new HashSet<>();
-            } else if (c == '}') {
+                partList = new ArrayList<>();
+            } else if (stringEle.equals("}")) {
                 // 집합 끝
-                partSet.add(ele);
+                partList.add(ele);
                 ele = 0;
-                totalSet.add(partSet);
-            } else if (c == ',') {
+                totalList.add(partList);
+            } else if (stringEle.equals(",")) {
                 // 다음 숫자
-                partSet.add(ele);
+                partList.add(ele);
                 ele = 0;
             } else {
                 // 숫자
                 ele *= 10;
-                ele += Integer.parseInt(c);
+                ele += Integer.parseInt(stringEle);
             }
         }
-        System.out.println(totalSet);
+        System.out.println("totalSet = " + totalList);
+        // 집합을 크기 순서대로 정렬
+        totalList.sort(new Comparator<ArrayList<Integer>>() {
+            @Override
+            public int compare(ArrayList<Integer> a, ArrayList<Integer> b) {
+                return Integer.compare(a.size(), b.size());
+            }
+        });
+        System.out.println("Sorted totalSet = " + totalList);
+        for (ArrayList<Integer> set : totalList) {
+            set.removeAll(result);
+            result.addAll(set);
+        }
+
         int[] answer = new int[result.size()];
         for (int i = 0; i < result.size(); i++) {
             answer[i] = result.get(i);
