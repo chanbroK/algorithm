94647ff0f2af3e3d591c0f4ab22f3701b2ce2402
chanbroK <koc0819@gmail.com>
2022-04-28 00:12:18 +0900

    kakao intern 2019 징검다리 건너기

--- src/main/java/kakao/intern2019/Q4.java
+++ src/main/java/kakao/intern2019/Q4.java
@@ -10,23 +10,93 @@ public class Q4 {
         System.out.println("solution = " + solution(stones, k));
     }
 
+    ///////////////////////////
+//                  version 1
+    ////////////////////////////
+//    public static int solution(int[] stones, int k) {
+//        int answer = 0;
+//        ArrayList<Integer> stoneList = new ArrayList<>();
+//        for (int val : stones) {
+//            stoneList.add(val);
+//        }
+//        //최소 값 구하기
+//        Integer min = Integer.MAX_VALUE;
+//        for (Integer val : stoneList) {
+//            if (val < min) {
+//                min = val;
+//            }
+//        }
+//        // 최소 값 만큼의 사람은 무조건 지나감
+//        for (int i = 0; i < stoneList.size(); i++) {
+//            stoneList.set(i, stoneList.get(i) - min);
+//        }
+//        answer += min;
+//        System.out.println("min = " + min);
+//        System.out.println("stoneList = " + stoneList);
+//        int stopFlag = 0;
+//        while (true) {
+//            // 연속해서 0인 디딤돌의 수
+//            int cnt = 0;
+//            for (int i = 0; i < stoneList.size(); i++) {
+//                Integer cur = stoneList.get(i);
+//                if (cur > 0) {
+//                    cnt = 0;
+//                    stoneList.set(i, cur - 1);
+//                } else if (cur.equals(0)) {
+//                    cnt++;
+//                }
+//                if (cnt == k) {
+//                    stopFlag = 1;
+//                    break;
+//                }
+//            }
+//            if (stopFlag != 0) {
+//                break;
+//            }
+//            answer++;
+//            System.out.println("stoneList = " + stoneList);
+//        }
+//        return answer;
+    ///////////////////////////
+//                  version 2
+    ////////////////////////////
     public static int solution(int[] stones, int k) {
         int answer = 0;
-        while (true) {
-            int flag = 0;
-            for (int i = 0; i < stones.length; i++) {
-                if (stones[i] > 0) {
-                    flag = 0;
-                    stones[i]--;
-                } else if (stones[i] == 0) {
-                    flag++;
-                    
+        // 건널 수 있는 사람이라는 가상의 배열이 있다고 생각하고 이진탐색
+        // Binary Search start, end value
+        System.out.println("stones = " + Arrays.toString(stones));
+        int start = 0;
+        int end = Integer.MIN_VALUE;
+        for (int stone : stones) {
+            if (stone > end) {
+                end = stone;
+            }
+        }
+        System.out.println("start = " + start);
+        System.out.println("end = " + end);
+
+        while (start <= end) {
+            int mid = (end + start) / 2;
+            System.out.println("mid = " + mid);
+            int cnt = 0;
+            for (int stone : stones) {
+                if (stone <= mid) {
+                    cnt++;
+                } else {
+                    cnt = 0;
+                }
+                if (cnt >= k) {
+                    break;
                 }
             }
-            if (flag == k) {
-                break;
+            if (cnt >= k) {
+                // mid명 의 사람이 건너고 나서부터 다음 사람은 건널 수 없는 상황
+                answer = mid;
+                end = mid - 1;
+            } else {
+                // start명 보다 적은 사람은 건너는게 보장
+                start = mid + 1;
             }
-            answer++;
         }
         return answer;
     }
