555789d77039a5ba31ba40866aee59b6cbf7e455
chanbroK <koc0819@gmail.com>
2022-05-24 00:25:41 +0900

    SWExpertAcademy 1206 View

---
+++ src/main/java/SWExpertAcademy/Q1206.java
@@ -0,0 +1,29 @@
+package SWExpertAcademy;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Q1206 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = 10;
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int length = sc.nextInt();
+            int result = 0;
+            LinkedList<Integer> queue = new LinkedList<>();
+            for (int i = 0; i < length; i++) {
+                int input = sc.nextInt();
+                queue.add(input);
+                if (i >= 4) {
+                    int max = Math.max(queue.get(0), Math.max(queue.get(1), Math.max(queue.get(3), queue.get(4))));
+                    if (max < queue.get(2))
+                        result += queue.get(2) - max;
+                    queue.remove();
+                }
+            }
+            System.out.println("#" + test_case + " " + result);
+        }
+    }
+}
